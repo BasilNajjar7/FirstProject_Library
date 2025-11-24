6777ca9a5b635d6dba2aff39a008a8d1b573036d
basil <basilnajjar80370532@gmail.com>
2025-11-24 16:01:33 +0300

    Created project

---
+++ FirstProject_Library/FirstProject_Library/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstProject_Library.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+    }
+}
